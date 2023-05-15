4d44bcd56336b930d82880fd7d6aaab90e89d92f
VMalakhatka <124716327+VMalakhatka@users.noreply.github.com>
2023-05-15 19:09:28 +0200

    First home work

---
+++ HW1_08_05.sql
@@ -0,0 +1,5 @@
+use hr
+select * from employees where department_id =90;
+select * from employees where salary > 5000;
+select * from employees where last_name like 'L%';
+select * from departments where location_id = 1700;
